7d0c3493324cf26a38a9e0b6b48128a9d7de0ba5
Vanessa Andrade Figueiredo <vfigueiredo@student.dei.uc.pt>
2025-01-28 15:40:31 +0000

    Hiperligação das categorias no main

--- scriptMain.js
+++ scriptMain.js
@@ -9,3 +9,39 @@ function toggleForm() {
         form.style.display = 'none';
     }
 }
+
+
+/* Para fazer a hiperligação entre as categorias e os artigos, fazendo desaparecer os artigos de outras categorias */
+
+document.addEventListener('DOMContentLoaded', function() {
+    const categories = document.querySelectorAll('aside ul li');
+    const articles = document.querySelectorAll('.grid-item');
+
+    // Adiciona um event listener a cada categoria
+    categories.forEach(category => {
+        category.addEventListener('click', function() {
+
+            // Remove a classe 'categoria-ativa' de todos os itens
+            categories.forEach(cat => cat.classList.remove('categoria-ativa'));
+
+            // Adiciona a classe 'categoria-ativa' ao item clicado
+            this.classList.add('categoria-ativa');
+
+            // Obtém a categoria selecionada
+            const selectedCategory = this.id;
+
+            // Exibe ou oculta os artigos de acordo com a categoria selecionada
+            articles.forEach(article => {
+                const articleCategory = article.querySelector('.text-overlay p:nth-child(4)').textContent.split(': ')[1].toLowerCase();
+                if (articleCategory === selectedCategory) {
+                    article.style.display = 'block'; // Exibe os artigos da categoria selecionada
+                } else {
+                    article.style.display = 'none'; // Oculta os artigos de outras categorias
+                }
+            });
+        });
+    });
+});
+
+
+
