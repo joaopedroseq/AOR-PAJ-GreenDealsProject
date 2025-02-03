2c3c4e8f0f481846285a75886c71794b4ff3e6ae
Vanessa Andrade Figueiredo <vfigueiredo@student.dei.uc.pt>
2025-02-03 11:14:15 +0000

    Adicionados comentários em todas funções

--- scriptMain.js
+++ scriptMain.js
@@ -1,18 +1,17 @@
 w3.includeHTML(() =>  {
 
-    // Adiciona o evento de clique ao ícone de hambúrguer para alternar o aside
-    const hamburger = document.getElementById('hamburger');
-    hamburger.addEventListener('click', toggleAside);   
+    // Mensagem de boas-vindas
+    const welcomeMessage = document.getElementById('mensagem_boasVindas');
 
-    
-      const welcomeMessage = document.getElementById('mensagem_boasVindas');
-      // Verificar se há um nome de utilizador armazenado no Session Storage
-      const storedUsername = sessionStorage.getItem('username');
-      if (storedUsername) {
+    // Verificar se há um nome de utilizador armazenado no Session Storage
+    const storedUsername = sessionStorage.getItem('username');
+    if (storedUsername) {
+        // Se houver um nome de utilizador armazenado, exibe a mensagem de boas-vindas
         welcomeMessage.textContent = `Olá, ${storedUsername}!`;
-      }  
-      
-      function logout(){
+    }  
+    
+    // Função para fazer logout, removendo o nome de utilizador do Session Storage
+    function logout(){
         const username = document.getElementById('username').value;
         sessionStorage.removeItem('username');
         sessionStorage.removeItem('logged');
@@ -20,23 +19,30 @@ w3.includeHTML(() =>  {
         loginMessage.style.visibility = 'hidden';
         const login = document.getElementById('loginButton');
         login.style.visibility = 'visible';
-        window.location.href = "index.html";
-      }
-  
-    
+        // Redireciona para a página de login
+        window.location.href = "index.html"; 
+    }
+
+    // Adiciona o evento de clique ao botão de logout
+    const logoutButton = document.getElementById('logoutButton');
+    logoutButton.addEventListener('click', logout);
+
+    // Adiciona o evento de clique ao ícone de hambúrguer para alternar o aside
+    const hamburger = document.getElementById('hamburger');
+    hamburger.addEventListener('click', toggleAside); 
+
+    // Obtém o botão para adicionar produto
     const addButton = document.getElementById('add-product-btn');
-    // Get the <span> element that closes the modal
+
+     //Funções para adicionar um produto
+     var modalDetail = document.getElementById("modal-detail");
+     var form = document.getElementById("edit-product-form");
+
+    // Obtém o elemento <span> que fecha o modal
     var closeDetail = document.getElementsByClassName("close-detail")[0];
-    
     
     // Adiciona o evento de submit ao formulário para guardar o produto
     var saveProductBtn = document.getElementById("save-product");
-
-    //FUNÇõES PARA ADICIONAR PRODUTO
-    var modalDetail = document.getElementById("modal-detail");
-
-    var form = document.getElementById("edit-product-form");
-    
 
     // Adiciona o evento de clique ao botão para alternar o formulário
     addButton.addEventListener('click', function(event) {
@@ -45,17 +51,15 @@ w3.includeHTML(() =>  {
         modalDetail.style.display = "flex";
     });
 
-
-    // When the user clicks on <span> (x), close the modal
+    // Quando o utilizador clicar no <span> (x), fecha o modal
     closeDetail.onclick = function() {
         modalDetail.style.display = "none";
-      };  
+    };  
     
-    
-
     // Função para guardar o produto no local storage
     saveProductBtn.addEventListener('click', function(event) {
         event.preventDefault();
+        // Obtém os valores dos campos do formulário
         const nome = document.getElementById('edit-nome').value;
         const descricao = document.getElementById('edit-descricao').value;  
         const preco = document.getElementById('edit-preco').value;
@@ -63,8 +67,9 @@ w3.includeHTML(() =>  {
         const anunciante = document.getElementById('edit-anunciante').value;
         const localidade = document.getElementById('edit-localidade').value;
         const imagem = document.getElementById('edit-imagem').value;
-        const data = new Date();
+        const data = new Date(); // Data atual
 
+        // Validações dos campos do formulário
         if(nome.trim() === ""){
             alert("O nome do produto é de preenchimento obrigatório");
         }
@@ -75,7 +80,7 @@ w3.includeHTML(() =>  {
             alert("O preço do produto é de preenchimento obrigatório");
         }
         else if(!checkIfNumeric(preco)){
-            alert("Preço inserido inválido. Apena poderá escrever digitos e sem quaisquer símbolos");
+            alert("Preço inserido inválido. Apenas poderá escrever dígitos e sem quaisquer símbolos");
         }
         else if(anunciante.trim() === ""){
             alert("O nome do anunciante é de preenchimento obrigatório");
@@ -84,9 +89,11 @@ w3.includeHTML(() =>  {
             alert("A sua morada é preenchimento obrigatório");
         }
         else if(imagem.trim() === ""){
-            alert("Terá de colocar um url válido da imagem do produto que pretenede anunciar");
+            alert("Terá de colocar um URL válido da imagem do produto que pretende anunciar");
         }
         else {
+
+            // Cria um objeto com os dados do produto
             const product = {
                 nome,
                 descricao,
@@ -97,48 +104,50 @@ w3.includeHTML(() =>  {
                 imagem,
                 data
             }
-            var confirm = window.confirm('Tem a certeza que pretende adicionar o produto' + nome + '?');
+            var confirm = window.confirm('Tem a certeza que pretende adicionar o produto ' + nome + '?'); 
+
+            // Se o utilizador confirmar, guarda o produto no local storage
             if(confirm == true){
                 console.log("salvar");
                 let products = localStorage.getItem('products');
                 if (products) {
-                    products = JSON.parse(products);
+                    // Converte a string JSON para um array
+                    products = JSON.parse(products); 
                 } else {
                     products = [];
                 }
                 products.push(product);
-                localStorage.setItem('products', JSON.stringify(products));
+                // Converte o array para uma string JSON
+                localStorage.setItem('products', JSON.stringify(products)); 
                 form.reset();
                 modalDetail.style.display = "none";
                 window.location.reload();
             }
+            // Se o utilizador cancelar, limpa o formulário e fecha o modal
             else {
                 form.reset();
                 modalDetail.style.display = "none";
             }
              
-        
+            // Exibe o produto na página
+            displayProduct(product, products.length - 1);
+        }
+    });
 
-        // Exibe o produto na página
-        displayProduct(product, products.length - 1);
-    }
-});
-
-    //função para verificar se um dado string é um número
+    // Função para verificar se uma string é um número
     function checkIfNumeric(string) {
-        return !isNaN(string) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
-               !isNaN(parseFloat(string)) // ...and ensure strings of whitespace fail
-      };
-      
+        return !isNaN(string) && // Usa coerção de tipo para analisar toda a string
+               !isNaN(parseFloat(string)) // Garante que strings de espaços em branco falhem
+    };
 
-   
-
+    // Obtém o container dos produtos
     const gridContainer = document.querySelector('.grid-container');
 
     // Função para exibir os produtos na página
     function displayProduct(product, index) {
+        // Cria o HTML do produto
         const productHTML = `
-            <div class="grid-item" onclick="window.location.href='detail.html?index=${index}'">
+            <div class="grid-item" onclick="window.location.href='detail.html?index=${index}'"> 
                 <img src='${product.imagem}' alt="${product.nome}"/>
                 <div class="text-overlay">
                     <h2 style="background-color: transparent;">${product.nome}</h2>
@@ -147,14 +156,15 @@ w3.includeHTML(() =>  {
                 </div>
             </div>
         `;
-        gridContainer.insertAdjacentHTML('beforeend', productHTML);
+        // Insere o produto no container
+        gridContainer.insertAdjacentHTML('beforeend', productHTML); 
     }
-
 
     // Função para carregar os produtos do local storage e exibi-los na página
     function loadProducts() {
         let products = localStorage.getItem('products');
         if (products) {
+            // Converte a string JSON para um array
             products = JSON.parse(products);
             products.forEach((product, index) => {
                 displayProduct(product, index);
@@ -172,45 +182,35 @@ w3.includeHTML(() =>  {
     // Adiciona um event listener a cada categoria
     categories.forEach(category => {
         category.addEventListener('click', function() {
-            
-
             // Remove a classe 'categoria-ativa' de todos os itens
             categories.forEach(cat => cat.classList.remove('categoria-ativa'));
-            
-
             // Adiciona a classe 'categoria-ativa' ao item clicado
             this.classList.add('categoria-ativa');
-            
-
             // Obtém a categoria selecionada
             const selectedCategory = this.id;
-            
-
             // Exibe ou oculta os artigos de acordo com a categoria selecionada
             articles.forEach(article => {                
                 const articleCategory = article.querySelector('.text-overlay p:nth-child(3)').textContent.split(': ')[1].toLowerCase();
                 if (articleCategory === selectedCategory) {
-                    article.style.display = 'block'; // Exibe os artigos da categoria selecionada
+                    // Exibe os artigos da categoria selecionada
+                    article.style.display = 'block'; 
                 } else {
-                    article.style.display = 'none'; // Oculta os artigos de outras categorias
+                    // Oculta os artigos de outras categorias
+                    article.style.display = 'none'; 
                 }
             });
         });
     });
-      // Função para alternar a exibição do aside
+
+    // Função para alternar a exibição do aside
     function toggleAside() {
         const asideMenu = document.getElementById("aside-menu");
+        // Se o aside estiver oculto ou não visível, exibe-o
         if (asideMenu.style.display === 'none' || asideMenu.style.display === '') {
             asideMenu.style.display = 'block';
         } else {
+            // Se o aside estiver visível, oculta-o
             asideMenu.style.display = 'none';
         }
-        
     }
-
-    
-    
-    const logoutButton = document.getElementById('logoutButton');
-    logoutButton.addEventListener('click', logout);
-
 });
